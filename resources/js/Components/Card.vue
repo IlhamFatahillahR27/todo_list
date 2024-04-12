3b392ac4a38f078566574958bf0ecb8947f6f8c2
ilhamfatahillahr <ilhamfatahillahr@gmail.com>
2024-04-12 09:58:29 +0700

    component card

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card :variant="variant ?? 'outlined'" :class="classCard">
+        <v-card-item v-if="title || subtitle">
+            <v-card-title>
+                {{ title }}
+                <slot name="title-card"/>
+            </v-card-title>
+            <v-card-subtitle>
+                {{ subtitle }}
+                <slot name="subtitle-card"/>
+            </v-card-subtitle>
+        </v-card-item>
+        <v-card-text>
+            <!-- in template do this -> '#body-card' to use this slot -->
+            {{ text }}
+            <slot name="body-card"/>
+        </v-card-text>
+    </v-card>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    title: String,
+    subtitle: String,
+    variant: String,
+    classCard: String,
+    text: String,
+})
+</script>
